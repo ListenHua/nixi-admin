e1876a5a058c887dd1042889fd4de6a72545f0e0
ListenHua <listenhua@ListenHuadeMacBook-Air.local>
2022-07-29 16:08:18 +0800

    fix:完善生成二维码

--- uniCloud-aliyun/cloudfunctions/public/index.js
+++ uniCloud-aliyun/cloudfunctions/public/index.js
@@ -17,21 +17,45 @@ exports.main = async (event, context) => {
 };
 
 async function qrcode(event) {
-	let access = await uniCloud.httpclient.request(
-		`https://api.weixin.qq.com/cgi-bin/token?appid=${appId}&secret=${appSecret}&grant_type=client_credential`, {
-			dataType: "json"
-		}
-	)
-	let accessToken = access.data.access_token
+	let timestamp = new Date().getTime()
+	const collection = db.collection('serverCache')
+	let serverAccess = (await collection.where({
+		type: "accessToken"
+	}).get()).data[0]
+	let accessToken;
+	if (serverAccess.time - 10000 < timestamp) {
+		let access = await uniCloud.httpclient.request(
+			`https://api.weixin.qq.com/cgi-bin/token?appid=${appId}&secret=${appSecret}&grant_type=client_credential`, {
+				dataType: "json"
+			}
+		)
+		accessToken = access.data.access_token
+		collection.doc("62e34b005c1f6c00015e1e42").update({
+			time: timestamp,
+			accessToken,
+		})
+	} else {
+		accessToken = serverAccess.accessToken
+	}
+	let params = {
+		"page": "pages/index/index",
+		"scene": "a=1",
+		"check_path": true,
+		"env_version": "release"
+	}
 	const wxdata = await uniCloud.httpclient.request(
-		`https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode`, {
-			method:"POST",
-			data:{
-				path:"?uid=admin",
-				access_token:accessToken
-			},
-			dataType: "json"
+		`https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=${accessToken}`, {
+			method: "POST",
+			data: JSON.stringify(params),
 		}
 	)
-	console.log('wxdata',wxdata);
+	let image = await uniCloud.uploadFile({
+		cloudPath: 'qrcode' + timestamp + '.jpg',
+		fileContent: wxdata.data
+	})
+	return {
+		code: 200,
+		msg: "请求成功",
+		data: image.fileID
+	}
 }
